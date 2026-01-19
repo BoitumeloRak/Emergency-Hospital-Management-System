aefff5e82aaeb0e001c9f5ce2cb4909dff872a37
borakgojhb024 <borakgojhb024@student.wethinkcode.co.za>
2026-01-19 11:33:07 +0200

    feat: added ResourceService consumer logic

---
+++ resource/src/main/java/hospital/resource/ResourceService.java
@@ -0,0 +1,65 @@
+package hospital.resource;
+
+import hospital.common.HospitalMQ;
+import hospital.common.TriageEvent;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/*
+ * This service deos not need a web server (like Javalin) necessarily,
+ * but it must stay running so it can listen for messages from ActiveMQ
+ */
+public class ResourceService {
+    public static void main(String[] args) {
+        try {
+            // 1. Connection to the ActiveMQ
+            ConnectionFactory factory = new ActiveMQConnectionFactory(HospitalMQ.BROKER_URL);
+
+            // NB: we must trust the 'hospital' package to allow sending objects
+            ((ActiveMQConnectionFactory) factory).setTrustAllPackages(true);
+
+            Connection connection = factory.createConnection();
+            connection.start();
+
+            // 2. Create a session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // 3. Create Destination (the same Topic used by Triage)
+            Topic destination = session.createTopic(HospitalMQ.TRIAGE_TOPIC);
+
+            // 4. Create a consumer
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            System.out.println("Resource Service is waiting for patients...");
+
+            // 5. Set up a Listener (This code runs every time a message arrives)
+            consumer.setMessageListener(message -> {
+                if (message instanceof ObjectMessage) {
+                    try {
+                        TriageEvent event = (TriageEvent) ((ObjectMessage) message).getObject();
+                        processPatient(event);
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("Resource Service Error: " + e.getMessage());
+        }
+    }
+
+    private static void processPatient(TriageEvent event) {
+        System.out.println("\n New Patient Alert");
+        System.out.println("Patient: " + event.getPaitentName());
+        System.out.println("Priority: " + event.getTriageLevel());
+
+        // Simple logic to "assign" resources based on priority
+        if (event.getTriageLevel().equalsIgnoreCase("RED")) {
+            System.out.println("Action: Assigning Emergency Room and Ventilator immediately");
+        } else {
+            System.out.println("Action: Adding to regular ward waiting list");
+        }
+
+    }
+}
