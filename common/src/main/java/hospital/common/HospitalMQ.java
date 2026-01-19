9c424b6b41f31dab1f7f16338f678c1b2420c337
borakgojhb024 <borakgojhb024@student.wethinkcode.co.za>
2026-01-19 09:14:38 +0200

    feat: implemented TriageEvent and HospitalMQ

--- common/src/main/java/hospital/common/HospitalMQ.java
+++ common/src/main/java/hospital/common/HospitalMQ.java
@@ -6,4 +6,5 @@ package hospital.common;
 public interface HospitalMQ {
     String BROKER_URL = "tcp://localhost:61616"; // Standard ActiveMQ connection URL
     String TRIAGE_TOPIC = "hospital.triage.updates"; // The topic name all services will use to talk about triage
+
 }
